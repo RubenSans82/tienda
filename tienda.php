a0206d54dc8a2db6f730c24f407b7c12969e0497
RubenSans82 <rubensans82@gmail.com>
2025-01-27 20:26:59 +0100

    menu tienda

--- tienda.php
+++ tienda.php
@@ -2,10 +2,9 @@
 session_start();
 if (isset($_SESSION["username"])) {
     $username = $_SESSION["username"];
-}
-else{
-    header("Location: index.php");
-    exit;
+} else {
+    header("Location: index");
+    exit();
 }
 ?>
 
@@ -19,24 +18,33 @@ else{
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/tienda.css">
 </head>
 
 <body>
-    <header>
-        <i class="fa-solid fa-store"></i>
-        <p><?php echo $_SESSION["username"]; ?></p>
-    </header>
-    <main>
-        <aside>
+    <div class="container">
+        <header>
+            <i class="fa-solid fa-store"></i>
+            <p><?php echo $_SESSION["username"]; ?></p>
+        </header>
+        <main>
+            <aside>
+                <ul>
+                    <li><a href="tienda"><i class="fa-solid fa-store"></i>Inicio</a></li>
+                    <li><a href="proveedores"><i class="fa-solid fa-boxes-packing"></i>Proveedores</a></li>
+                    <li><a href="clientes"><i class="fa-regular fa-address-card"></i></i>Clientes</a></li>
+                    <li><a href="categorias"><i class="fa-solid fa-layer-group"></i>Categorias</a></li>
+                    <li><a href="productos"><i class="fa-solid fa-warehouse"></i>Productos</a></li>
+                    <li><a href="ventas"><i class="fa-brands fa-sellsy"></i>Ventas</a></li>
+                </ul>
+            </aside>
+            <section>
 
-        </aside>
-        <section>
+            </section>
+        </main>
+        <footer></footer>
+    </div>
 
-        </section>
-    </main>
-    <footer>
-
-    </footer>
 </body>
 
 </html>
