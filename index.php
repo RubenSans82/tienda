708086e840384d240f278ce41f2b6eeb1c99f7c1
RubenSans82 <rubensans82@gmail.com>
2025-01-27 16:47:22 +0100

    index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post"></form>
+    <label for="username">Nombre de usuario</label>
+    <input type="text" name="username" id="username" required placeholder="username">
+    <label for="password">Contraseña</label>
+    <input type="password" name="password" id="password" required placeholder="password">
+    <input type="submit" value="Iniciar sesión">
+</body>
+</html>
