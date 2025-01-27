e388e4f9a23a5e5b2f99efb0ed441a52260e0f8c
RubenSans82 <rubensans82@gmail.com>
2025-01-27 19:28:57 +0100

    captura de excepcion

--- conexiondb.php
+++ conexiondb.php
@@ -5,13 +5,11 @@ $user = DB_USER;
 $password = DB_PASS;
 $database = DB_DATABASE;
 
-try {
     // Crear la conexión con PDO
     $conexion = new PDO("mysql:host=$host;dbname=$database", $user, $password);
     // Establecer el modo de error de PDO a excepción
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Conexión exitosa";
-} catch (PDOException $e) {
-    echo "Conexión fallida: " . $e->getMessage();
-}
+
+
 ?>
